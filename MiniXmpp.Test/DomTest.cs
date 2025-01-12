ab26336254fc72bf592f53c90a0bfd6ce92b55ae
Nathan Ferreira <14365254+nathan130200@users.noreply.github.com>
2025-01-12 20:24:01 -0300

    add test project fix for element parser (wrong value was set)

---
+++ MiniXmpp.Test/DomTest.cs
@@ -0,0 +1,16 @@
+﻿namespace MiniXmpp.Test;
+
+public class DomTest
+{
+    [Fact]
+    public void SimpleElement()
+    {
+        var el = new XmppElement("foo")
+        {
+            Attributes =
+            {
+                ["bar"] = "baz"
+            }
+        };
+    }
+}
